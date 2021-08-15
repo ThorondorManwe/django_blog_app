7bd4475090ded1423b44a704c6af62c3f98224b0
Carlos Rangel <charlesleimaginant@gmail.com>
2021-08-14 21:53:05 -0500

    Changed settings.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -11,8 +10,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               reloated_name='blog_posts')
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'blog.apps.BlogConfig',
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
