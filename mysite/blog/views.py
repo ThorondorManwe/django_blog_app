cd149bdde2e23608d124d75e32164c06233517fd
Carlos Rangel <charlesleimaginant@gmail.com>
2021-09-01 19:47:11 -0500

    Changed views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -65,6 +65,7 @@ def post_detail(request, year, month, day, post):
                                   .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                                 .order_by('-same_tags','-publish')[:4]
+    
 
     return render(request,
                   'blog/post/detail.html',
